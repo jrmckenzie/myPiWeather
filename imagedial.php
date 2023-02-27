66b22c2bca10d257937833c28d1f899f8442d97c
jrmckenzie <75369880+jrmckenzie@users.noreply.github.com>
2023-02-27 13:56:01 +0000

    Update imagedial.php
    Update it to use a font for the pointer which can be rotated easily

--- imagedial.php
+++ imagedial.php
@@ -1,18 +1,19 @@
 <?php
-// Set path to directory where user installed myPiWeather
-$myPiWeatherDir = "/home/pi/myPiWeather";
 
-// Set fonts and images to be used
-$ttfont = "$myPiWeatherDir/Roboto-Medium.ttf";
-$pointerGif = "$myPiWeatherDir/pointer.gif";
-$facePng = "$myPiWeatherDir/face_hPa.png";
+// Copyright (C) 2023 James McKenzie jrmknz@yahoo.co.uk
+// Set up fonts and images to be used
+$facePng = "barometer_600.png";
+$textFont = "Rubik-Medium.ttf";
+$pointerFont = "barometer.ttf";
+$textFontsize = 40;
+$pointerFontsize = 910;
 
 // Get pressure and rate of change (slope) from GET input vars
 $mb = floatval(trim($_GET['mb']));
 $slope = floatval(trim($_GET['slope']));
 
 // Calculate pointer angle from pressure
-$angle = 1.771798287*(1049.7809-$mb);
+$angle = 1.8*(1050-$mb);
 
 // Describe rate of change of pressure
 if ($slope > 6) { $change = "RISING VERY RAPIDLY"; }
@@ -27,48 +28,33 @@ elseif ($slope > -3.55) { $change = "FALLING"; }
 elseif ($slope >= -6) { $change = "FALLING QUICKLY"; }
 elseif ($slope < -6) { $change = "FALLING VERY RAPIDLY"; }
 
-// Load pointer graphic
-$source = imagecreatefromgif($pointerGif) or die("Couldn't load and create pointer graphic from gif file!  Is $pointerGif a valid, readable gif file?");
-$sw = imagesx($source);
-$sh = imagesy($source);
+// Load face (recommended to be square and 600px x 600px or 1200px x 1200px)
+$face = imagecreatefrompng($facePng) or die("Couldn't load face graphic from png file! Is $facePng a valid, readable png file?");
+$faceW = imagesx($face);
+$faceH = imagesy($face);
+$textFontsize = intval($textFontsize * ($faceW / 1200));
+$pointerFontsize = intval($pointerFontsize * ($faceW / 1200));
 
-//  Rotate pointer to required angle
-$rotate = imagerotate($source, $angle, 128);
-imagedestroy($source);
-$rw = imagesx($rotate);
-$rh = imagesy($rotate);
-$crop = imagecrop($rotate, array(
-    'x' => $rw * (1 - $sw / $rw) * 0.5,
-    'y' => $rh * (1 - $sh / $rh) * 0.5,
-    'width' => $sw,
-    'height'=> $sh
-));
-imagedestroy($rotate);
-$white = imagecolorallocate($crop, 255, 255, 255);
-imagecolortransparent($crop, $white);
-imagefilter($crop, IMG_FILTER_NEGATE);
-imagefilter($crop, IMG_FILTER_BRIGHTNESS, 32);
+// Load font and add pointer and text describing rate of change of pressure
+$black = imagecolorallocate($face, 0, 0, 0);
+$tb = imagettfbbox($textFontsize, 0, $textFont, $change) or die("Couldn't calculate dimensions of text box! Is $textFont a valid, readable truetype font file?");
+$textXpos = ceil(($faceW - $trimPx * 2 - $tb[2]) / 2);
+$textYpos = $faceH - $trimPx * 2 - 10;
+imagettftext($face, $textFontsize, 0, $textXpos, $textYpos, $black, $textFont, $change);
+imagettftext($face, $pointerFontsize, $angle, intval($faceW / 2), intval($faceH / 2), $black, $pointerFont, '-') or die("Couldn't draw pointer! Is $pointerFont a valid, readable truetype font file?");
 
-// Load face (600px x 600px) and superimpose pointer
-$face = imagecreatefrompng($facePng) or die("Couldn't load and create face graphic from png file! Is $facePng a valid, readable png file?");
-$merged = imagecopymerge($face,$crop,0,0,0,0,600,600,100);
-imagedestroy($crop);
+// Dark mode
+imagefilter($face, IMG_FILTER_NEGATE);
 
-// Trim 6px off borders of image - face will then be 588px x 588px
-$image = imagecrop($face,['x' => 6,'y' => 6,'width' => 588,'height' => 588]);
-imagedestroy($face);
-
-// Load font and add text describing rate of change of pressure
-$fontsize = 20;
-$black = imagecolorallocate($image, 30, 30, 36);
-$tb = imagettfbbox($fontsize, 0, $ttfont, $change) or die("Couldn't calculate dimensions of text box! Is $ttfont a valid, readable truetype font file?");
-$textXpos = ceil((588 - $tb[2]) / 2);
-$textYpos = 578;
-imagettftext($image, $fontsize, 0, $textXpos, $textYpos, $black, $ttfont, $change);
+// Trim $trimPx pixels off borders of image
+$trimPx = 0;
+if ($trimPx > 0) {
+    $face = imagecrop($face,['x' => $trimPx,'y' => $trimPx,'width' => $faceW - $trimPx * 2,'height' => $faceH - $trimPx * 2]);
+}
 
 // Return image to browser
 header( "Content-type: image/png" );
-imagepng($image);
-imagedestroy($image);
+imagepng($face);
+imagedestroy($face);
 
 ?>
